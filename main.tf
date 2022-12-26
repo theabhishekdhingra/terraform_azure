4e3e2fa4a98b96e55511a7bbfdff1ce18a8cd832
divyatradeacc <divya.tradeacc@gmail.com>
2022-12-26 10:02:14 +0000

    adding lock file

--- main.tf
+++ main.tf
@@ -1,1 +1,14 @@
 # my first terraform main.tf
+
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.37.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  # Configuration options
+}
